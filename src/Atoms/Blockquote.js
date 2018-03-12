9717cf9ffecfdd6702327fba810b43c74cd90a9a
Will Vatcher <will.vatcher@gmail.com>
2018-03-11 21:25:10 -0700

    Adding Atoms

---
+++ src/Atoms/Blockquote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Blockquote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            
+         )
+    }
+}
+ 
+export default Blockquote;
